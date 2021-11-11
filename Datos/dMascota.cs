e6efe6424e3967308c00054677cff11e260fb86d
Alvaro Lamg <alvarolamg@gmail.com>
2021-11-11 13:35:03 -0500

    Mascota datos y negocio

---
+++ Datos/dMascota.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Entidades;
+
+namespace Datos
+{
+    public class dMascota
+    {
+        Database db = new Database();
+        public void Insertar(eMascota o)
+        {
+            try
+            {
+                SqlConnection con = db.ConectaDb();
+                string insert = "Insertar_Mascota";
+                SqlCommand cmd = new SqlCommand(insert, con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@nombre", o.Nombre);
+                cmd.Parameters.AddWithValue("@tipo", o.Tipo);
+                cmd.Parameters.AddWithValue("@edad", o.Edad);
+                cmd.ExecuteNonQuery();
+                cmd.Parameters.Clear();
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+            finally
+            {
+                db.DesconectaDb();
+            }
+        }
+        public void Modificar(eMascota o)
+        {
+            try
+            {
+                SqlConnection con = db.ConectaDb();
+                string update = "Modificar_Mascota";
+                SqlCommand cmd = new SqlCommand(update, con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@nombre", o.Nombre);
+                cmd.Parameters.AddWithValue("@tipo", o.Tipo);
+                cmd.Parameters.AddWithValue("@edad", o.Edad);
+                cmd.Parameters.AddWithValue("@id", o.Id);
+                cmd.ExecuteNonQuery();
+                cmd.Parameters.Clear();
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+            finally
+            {
+                db.DesconectaDb();
+            }
+        }
+        public void Eliminar(int id)
+        {
+            try
+            {
+                SqlConnection con = db.ConectaDb();
+                string delete = "Eliminar_Mascota";
+                SqlCommand cmd = new SqlCommand(delete, con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                cmd.Parameters.Clear();
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+            finally
+            {
+                db.DesconectaDb();
+            }
+        }
+        public List<eMascota> ListarTodo()
+        {
+            try
+            {
+                List<eMascota> lsMascota = new List<eMascota>();
+                eMascota mascota = null;
+                SqlConnection con = db.ConectaDb();
+                SqlCommand cmd = new SqlCommand("Mostrar_Mascota", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    mascota = new eMascota();
+                    mascota.Id = (int)reader["id"];
+                    mascota.Nombre = (string)reader["nombre"];
+                    mascota.Tipo = (string)reader["tipo"];
+                    mascota.Edad = (int)reader["edad"];
+                    lsMascota.Add(mascota);
+                }
+                reader.Close();
+                return lsMascota;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                db.DesconectaDb();
+            }
+        }
+    }
+}
---
+++ Negocio/nMascota.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using Datos;
+
+namespace Negocio
+{
+    class nMascota
+    {
+        dMascota mascotadao;
+        public nMascota()
+        {
+            mascotadao = new dMascota();
+        }
+        public void RegistrarMascota(string nombre, string tipo, int edad)
+        {
+            eMascota mascota = new eMascota()
+            {
+                Nombre = nombre,
+                Tipo = tipo,
+                Edad = edad
+            };
+            mascotadao.Insertar(mascota);
+        }
+        public void ModificarMascota(int id, string nombre, string tipo, int edad)
+        {
+            eMascota mascota = new eMascota()
+            {
+                Id = id,
+                Nombre = nombre,
+                Tipo = tipo,
+                Edad = edad
+            };
+            mascotadao.Modificar(mascota);
+        }
+        public void EliminarMascota(int id)
+        {
+            mascotadao.Eliminar(id);
+        }
+        public List<eMascota> ListarMascota()
+        {
+            return mascotadao.ListarTodo();
+        }
+    }
+}
